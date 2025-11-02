44846585310577a265a4a8ac83dea8f3503fce9c
Hema <Hema@DESKTOP-7LK21OC>
2025-11-02 13:10:10 +0200

    Add project files.

---
+++ E-Commerce.API/Program.cs
@@ -0,0 +1,51 @@
+
+using Domain.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Presistence.Data;
+using System.Threading.Tasks;
+
+namespace E_Commerce.API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+            });
+            builder.Services.AddScoped<IDataSeeding, DataSeeding>();
+
+            var app = builder.Build();
+
+            #region Data Seed Before any request
+            using var scope = app.Services.CreateScope();
+            var ObjectOfDataSeeding = scope.ServiceProvider.GetRequiredService<IDataSeeding>();
+            await ObjectOfDataSeeding.SeedDataAsync(); 
+            #endregion
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ Infrastructure/Presistence/Data/DataSeeding.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Presistence.Data
+{
+    public class DataSeeding(StoreDbContext _dbContext) : IDataSeeding
+    {
+        public async Task SeedDataAsync()
+        {
+            try
+            {
+                var pendingMigrations = await _dbContext.Database.GetPendingMigrationsAsync();
+                if (pendingMigrations.Any())
+                {
+                    await _dbContext.Database.MigrateAsync();
+                }
+                if (!_dbContext.ProductBrands.Any())
+                {
+                    var productBrandsData = File.OpenRead("..\\Infrastructure\\Presistence\\Data\\DataSeed\\brands.json");
+                    var productBrands = await JsonSerializer.DeserializeAsync<List<ProductBrand>>(productBrandsData);
+                    if (productBrands is not null && productBrands.Any())
+                        await _dbContext.ProductBrands.AddRangeAsync(productBrands);
+                }
+                if (!_dbContext.ProductTypes.Any())
+                {
+                    var productTypesData = File.OpenRead("..\\Infrastructure\\Presistence\\Data\\DataSeed\\types.json");
+                    var productTypes = await JsonSerializer.DeserializeAsync<List<ProductType>>(productTypesData);
+                    if (productTypes is not null && productTypes.Any())
+                        await _dbContext.ProductTypes.AddRangeAsync(productTypes);
+                }
+                if (!_dbContext.Products.Any())
+                {
+                    var productsData = File.OpenRead("..\\Infrastructure\\Presistence\\Data\\DataSeed\\products.json");
+                    var products = await JsonSerializer.DeserializeAsync<List<Product>>(productsData);
+                    if (products is not null && products.Any())
+                        await _dbContext.Products.AddRangeAsync(products);
+                }
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                //DOTO
+            }
+        }
+    }
+}
