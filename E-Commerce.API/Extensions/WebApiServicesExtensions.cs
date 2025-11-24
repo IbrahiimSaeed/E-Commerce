460215f15c4808819b531d2a9a383d6668607938
Hema <Hema@DESKTOP-7LK21OC>
2025-11-24 18:30:32 +0200

    Authorization for Swagger

--- E-Commerce.API/Extensions/WebApiServicesExtensions.cs
+++ E-Commerce.API/Extensions/WebApiServicesExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using E_Commerce.API.Factories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 
 namespace E_Commerce.API.Extensions
 {
@@ -9,7 +10,32 @@ namespace E_Commerce.API.Extensions
         {
             services.AddControllers();   
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(option =>
+            {
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+            });
             services.Configure<ApiBehaviorOptions>(options =>
             {
                 options.InvalidModelStateResponseFactory = ApiResponseFactory.CustomValidationErrorResponse;
